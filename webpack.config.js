43f72e7168f64dce67db228285270edfb2dda50b
VForsmann <vitus.forsmann@hotmail.de>
2020-12-28 22:49:44 +0100

    change dir for bundle

--- webpack.config.js
+++ webpack.config.js
@@ -73,7 +73,7 @@ module.exports = {
     ],
     output: {
         filename: "[name].js",
-        path: path.resolve(__dirname, "./dist"),
+        path: path.resolve(__dirname, "./docs"),
     },
     devServer: {
         historyApiFallback: {
